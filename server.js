459077edfb9c4ef82f5d699d505595c6bd6c9a2d
Aaron Paden <aaronbpaden@gmail.com>
2022-03-17 09:30:50 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require('express');
+const server = express();
+const config = require('./config/config');
+
+server.set('view engine', 'ejs');
+server.listen(config.PORT, config.HOST, () => console.log(`Listening on ${config.HOST}:${config.PORT}`));
