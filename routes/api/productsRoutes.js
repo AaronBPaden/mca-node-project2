a51556ac8dd452185dba2f6beb0494b2f4c72e55
Aaron Paden <aaronbpaden@gmail.com>
2022-03-24 09:46:57 -0500

    Add single product route.

--- routes/api/productsRoutes.js
+++ routes/api/productsRoutes.js
@@ -17,16 +17,44 @@ const fetchProductsList = (URL, title, productType, res) => {
 		});
 };
 
+const fetchProduct = (URL, productType, res) => {
+	fetch(URL)
+		.then(res => res.json())
+		.then(data => {
+			data.productType = productType;
+			res.render('pages/single-product', {
+				title: data.name,
+				name: data.name,
+				data
+			});
+		});
+};
+
 router.get('/ale', (req, res) => {
 	fetchProductsList(`${API}/ale`, 'Ale Store', 'ale', res);
+});
+
+router.get('/ale/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	fetchProduct(`${API}/ale/${id}`, 'ale', res);
 });
 
 router.get('/stouts', (req, res) => {
 	fetchProductsList(`${API}/stouts`, 'Stouts Store', 'stouts', res);
 });
 
+router.get('/stouts/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	fetchProduct(`${API}/stouts/${id}`, 'stouts', res);
+});
+
 router.get('/red-ale', (req, res) => {
 	fetchProductsList(`${API}/red-ale`, 'Red-Ale Store', 'red-ale', res);
+});
+
+router.get('/red-ale/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	fetchProduct(`${API}/red-ale/${id}`, 'red-ale', res);
 });
 
 module.exports = router;
